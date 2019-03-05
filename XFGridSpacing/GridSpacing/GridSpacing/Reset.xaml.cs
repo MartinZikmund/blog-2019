21d1ba6586830b174e086bd9bd09c92ef8c391ef
Martin Zikmund <martinzikmund@live.com>
2019-03-05 20:02:56 +0100

    Xamarin.Forms Grid Spacing sample

---
+++ XFGridSpacing/GridSpacing/GridSpacing/Reset.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GridSpacing
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Reset : ResourceDictionary
+	{
+		public Reset ()
+		{
+			InitializeComponent();
+		}
+	}
+}
