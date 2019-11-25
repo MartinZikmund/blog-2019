d5cd6d3202a4acf0483ed840535c1f07d45264d6
Martin Zikmund <martinzikmund@live.com>
2019-11-25 22:15:36 +0100

    UWP appsettings.json example

---
+++ UwpAppSettingsJson/Config/AppConfig.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UwpAppSettingsJson.Config.Sections;
+using Windows.ApplicationModel;
+
+namespace UwpAppSettingsJson.Config
+{
+    public class AppConfig
+    {
+        private readonly IConfigurationRoot _configurationRoot;
+
+        public AppConfig()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Package.Current.InstalledLocation.Path)
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile("appsettings.debug.json", optional: true);
+
+            _configurationRoot = builder.Build();
+        }
+
+        public Example Example => GetSection<Example>(nameof(Example));
+
+        private T GetSection<T>(string key) => _configurationRoot.GetSection(key).Get<T>();
+    }
+}
---
+++ UwpAppSettingsJson/Config/Sections/Example.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UwpAppSettingsJson.Config.Sections
+{
+    public class Example
+    {
+        public string Message { get; set; }
+    }
+}
