362fe4822595d3717f40bf71a0fe10f337b613f1
Martin Zikmund <martinzikmund@live.com>
2019-12-08 23:10:41 +0100

    Christmas riddle

---
+++ ChristmasRiddle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Riddle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var valuesArray = new ValueHolder[] { new ValueHolder() };
+            valuesArray[0].SetValue(5);
+
+            var valuesList = new List<ValueHolder>() { new ValueHolder() };
+            valuesList[0].SetValue(5);
+
+            Console.WriteLine(valuesList[0].Value == valuesArray[0].Value);
+        }
+    }
+
+    public struct ValueHolder
+    {
+        public int Value { get; private set; }
+
+        public void SetValue(int value) => Value = value;
+    }
+}
